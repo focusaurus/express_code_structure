ce2d538316554676df690dc4f531101bb7703a37
Peter Lyons <pete@peterlyons.com>
2013-10-27 10:27:12 -0600

    tiny commits while I test mirroring to github

--- app/server.js
+++ app/server.js
@@ -2,3 +2,13 @@ var express = require("express");
 var app = express();
 
 app.set("views", __dirname);
+
+//Load the routes ("controllers" -ish)
+[
+  "app/users/routes",
+  "app/vehicles/routes",
+  "app/customers/routes",
+  "app/deals/routes"
+].forEach(function (routePath) {
+    require(routePath)(app);
+});
